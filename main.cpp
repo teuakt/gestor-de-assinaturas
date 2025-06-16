a6d7ba1fc507c38c36015b90242708bf5f2a1ceb
teuakt <mateushfsouza2@gmail.com>
2025-06-16 20:05:45 -0300

    test: add temporary main.cpp to test Gestor functionalities

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include "include/gestor.h"
 #include "include/assinatura.h"
 
 using namespace std;
 
 int main() {
-    Assinatura netflix("Netflix", 39.90, 15);
+    Gestor gestor;
 
-    netflix.exibir();
+    Assinatura netflix("Netflix", 39.90, 15);
+    Assinatura spotify("Spotify", 19.90, 10);
+
+    gestor.adicionarAssinatura(netflix);
+    gestor.adicionarAssinatura(spotify);
+
+    cout << "Assinaturas cadastradas:" << endl;
+    gestor.listarAssinaturas();
+
+    gestor.removerAssinatura("Spotify");
+
+    cout << "\nApós remover Spotify:" << endl;
+    gestor.listarAssinaturas();
 
     return 0;
 }
-
