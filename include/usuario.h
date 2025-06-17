4cb8ffa434be6b91977204842a372c2465bdb9e2
teuakt <mateushfsouza2@gmail.com>
2025-06-16 21:32:57 -0300

    feat(usuario): add load and save functions for user data persistence

--- include/usuario.h
+++ include/usuario.h
@@ -1,22 +1,23 @@
-#ifndef GESTOR_H
-#define GESTOR_H
+#ifndef USUARIO_H
+#define USUARIO_H
 
 #include "assinatura.h"
 #include <vector>
 #include <string>
 
-class Gestor {
+class Usuario {
 private:
     std::vector<Assinatura> assinaturas_;
 
 public:
     void adicionarAssinatura(const Assinatura& assinatura);
-
     bool removerAssinatura(const std::string& nome);
-
     bool editarAssinatura(const std::string& nome, const Assinatura& novaAssinatura);
-
     void listarAssinaturas() const;
+    double calcularGastoMensal() const;
+
+    bool salvarEmArquivo(const std::string& nomeArquivo) const;
+    bool carregarDeArquivo(const std::string& nomeArquivo);
 };
 
 #endif
