3403a8dad187d2d2ccba4c7f16a06eb4bd9949b3
teuakt <mateushfsouza2@gmail.com>
2025-06-17 17:41:06 -0300

    feat(gestor): add initial interface for menu and command processing

--- include/gestor.h
+++ include/gestor.h
@@ -1,22 +1,17 @@
 #ifndef GESTOR_H
 #define GESTOR_H
 
-#include "assinatura.h"
-#include <vector>
-#include <string>
+#include "usuario.h"
 
 class Gestor {
 private:
-    std::vector<Assinatura> assinaturas_;
+    Usuario usuario_;
 
 public:
-    void adicionarAssinatura(const Assinatura& assinatura);
-
-    bool removerAssinatura(const std::string& nome);
-
-    bool editarAssinatura(const std::string& nome, const Assinatura& novaAssinatura);
-
-    void listarAssinaturas() const;
+    Gestor();
+    
+    void exibirMenu(); 
+    void processarComando(int opcao); 
 };
 
 #endif
