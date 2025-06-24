dd28dcca5a935a2bec55c11003d27fa289c87753
teuakt <mateushfsouza2@gmail.com>
2025-06-23 22:25:50 -0300

    implement command inteface in Gestor class

--- src/gestor.cpp
+++ src/gestor.cpp
@@ -3,38 +3,103 @@
 
 using namespace std;
 
-void Gestor::adicionarAssinatura(const Assinatura& assinatura) {
-    assinaturas_.push_back(assinatura);
+Gestor::Gestor() {
+    usuario_.carregarDeArquivo("userData.txt");
 }
 
-bool Gestor::removerAssinatura(const string& nome) {
-    for (auto it = assinaturas_.begin(); it != assinaturas_.end(); ++it) {
-        if (it->getNome() == nome) {
-            assinaturas_.erase(it);
-            return true;
+void Gestor::exibirMenu() {
+    int opcao = -1;
+
+    while (opcao != 0) {
+        cout << "\n=== Gestor de Assinaturas de Streaming ===\n";
+        cout << "1. Adicionar assinatura\n";
+        cout << "2. Remover assinatura\n";
+        cout << "3. Editar assinatura\n";
+        cout << "4. Listar assinaturas\n";
+        cout << "5. Gerar relatorio de gastos\n";
+        cout << "0. Sair\n";
+        cout << "Escolha uma opção: ";
+        cin >> opcao;
+
+        cin.ignore();
+        processarComando(opcao);
+    }
+
+    cout << "Encerrando o programa. Até logo!\n";
+}
+
+void Gestor::processarComando(int opcao) {
+    switch (opcao) {
+        case 1: {
+            string nome;
+            double valor;
+            int dia;
+
+            cout << "Nome da assinatura: ";
+            getline(cin, nome);
+
+            cout << "Valor mensal (R$): ";
+            cin >> valor;
+
+            cout << "Dia de renovação (1-31): ";
+            cin >> dia;
+            cin.ignore();
+
+            Assinatura nova(nome, valor, dia);
+            usuario_.adicionarAssinatura(nova);
+            usuario_.salvarEmArquivo("userData.txt");
+            cout << "Assinatura adicionada com sucesso!\n";
+            break;
         }
-    }
-    return false;
-}
+        case 2: {
+            string nome;
+            cout << "Nome da assinatura a remover: ";
+            getline(cin, nome);
 
-bool Gestor::editarAssinatura(const string& nome, const Assinatura& novaAssinatura) {
-    for (auto& a : assinaturas_) {
-        if (a.getNome() == nome) {
-            a = novaAssinatura;
-            return true;
+            if (usuario_.removerAssinatura(nome)) {
+                usuario_.salvarEmArquivo("userData.txt");
+                cout << "Assinatura removida com sucesso!\n";
+            } else {
+                cout << "Assinatura não encontrada.\n";
+            }
+            break;
         }
-    }
-    return false;
-}
+        case 3: {
+            string nomeAntigo, nomeNovo;
+            double valorNovo;
+            int diaNovo;
 
-void Gestor::listarAssinaturas() const {
-    if (assinaturas_.empty()) {
-        cout << "Nenhuma assinatura cadastrada.\n";
-        return;
-    }
+            cout << "Nome da assinatura a editar: ";
+            getline(cin, nomeAntigo);
 
-    cout << "Assinaturas cadastradas:\n";
-    for (const auto& a : assinaturas_) {
-        a.exibir();
+            cout << "Novo nome: ";
+            getline(cin, nomeNovo);
+
+            cout << "Novo valor (R$): ";
+            cin >> valorNovo;
+
+            cout << "Novo dia de renovação: ";
+            cin >> diaNovo;
+            cin.ignore();
+
+            Assinatura nova(nomeNovo, valorNovo, diaNovo);
+            if (usuario_.editarAssinatura(nomeAntigo, nova)) {
+                usuario_.salvarEmArquivo("userData.txt");
+                cout << "Assinatura atualizada com sucesso!\n";
+            } else {
+                cout << "Assinatura não encontrada.\n";
+            }
+            break;
+        }
+        case 4:
+            usuario_.listarAssinaturas();
+            break;
+        case 5:
+            cout << "Gasto mensal total: R$ " << usuario_.calcularGastoMensal() << "\n";
+            break;
+        case 0:
+            break;
+        default:
+            cout << "Opção inválida. Tente novamente.\n";
     }
 }
