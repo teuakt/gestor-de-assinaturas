0c23fdff34949bf89f770d6e7dd4be898e65f0b4
teuakt <mateushfsouza2@gmail.com>
2025-06-27 09:15:09 -0300

    feat: display renewal reminders at startup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
-#include <fstream>
 #include "gestor.h"
-
-using namespace std;
+#include "lembrete.h"
+#include <iostream>
 
 int main() {
     Gestor gestor;
+
+    if (!gestor.carregarUsuario()) {
+        std::cout << "Erro ao carregar dados do usuário." << std::endl;
+    }
+
+    Lembrete lembrete(gestor.getUsuario());
+    lembrete.verificarRenovacoes(7);
+
     gestor.exibirMenu();
 
     return 0;
