c3c0f63342b1d18062cb29c548fcd99fb022ec3a
teuakt <mateushfsouza2@gmail.com>
2025-06-28 11:53:48 -0300

    refactor(lembrete): throw exception when subscription list is empty

--- src/lembrete.cpp
+++ src/lembrete.cpp
@@ -1,4 +1,5 @@
 #include "lembrete.h"
+#include "erros.h"
 #include <iostream>
 #include <ctime>
 #include <thread>
@@ -10,28 +11,30 @@ Lembrete::Lembrete(Usuario& usuario)
     : usuario_(usuario) {}
 
 void Lembrete::verificarRenovacoes(int diasAntecedencia) const {
+    const vector<Assinatura>& assinaturas = usuario_.getAssinaturas();
+
+    if (assinaturas.empty()) {
+        throw ListaDeAssinaturasVaziaException{};
+    }
+
     time_t tempoAtual = time(nullptr);
     tm* tempoLocal = localtime(&tempoAtual);
     int diaAtual = tempoLocal->tm_mday;
 
     cout << "\nVerificando renovações para os próximos " << diasAntecedencia << " dias..." << endl;
-    bool encontrou = false;
-
-    const vector<Assinatura>& assinaturas = usuario_.getAssinaturas();
-
+    
     for (const auto& assinatura : assinaturas) {
         int diaRenovacao = assinatura.getDiaRenovacao();
+        
         int diasRestantes = diaRenovacao - diaAtual;
+        if (diaRenovacao < diaAtual) {
+            diasRestantes += 30; 
+        }
+
         this_thread::sleep_for(chrono::milliseconds(500));
         if (diasRestantes >= 0 && diasRestantes <= diasAntecedencia) {
-            cout << "\nAtenção! A assinatura \"" << assinatura.getNome()
+            cout << "  -> Atenção! A assinatura \"" << assinatura.getNome()
                  << "\" será renovada em " << diasRestantes << " dia(s)!" << endl;
-            encontrou = true;
         }
-    }
-
-    if (!encontrou) {
-        this_thread::sleep_for(chrono::milliseconds(500));
-        cout << "Nenhuma renovação prevista para os próximos dias." << endl;
     }
 }
