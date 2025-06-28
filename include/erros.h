9075d6b7dbf9f8c58c6659ded5011d46aafed3e6
teuakt <mateushfsouza2@gmail.com>
2025-06-28 12:03:02 -0300

    feat(erros): Add all custom exceptions types for the project

--- include/erros.h
+++ include/erros.h
@@ -1,12 +1,37 @@
+// erros.h
 #ifndef ERROS_H
 #define ERROS_H
 
 #include <string>
 
-struct ErroAssinatura {
-    std::string mensagem;
-
-    ErroAssinatura(const std::string& msg) : mensagem(msg) {}
+// ---------------------------------- usuario --------------------------------- //
+struct AssinaturaJaExisteException {
+    const std::string nome;
 };
 
-#endif
+struct AssinaturaNaoEncontradaException {
+    const std::string nome;
+};
+
+struct ListaDeAssinaturasVaziaException {};
+
+struct ErroAoAbrirArquivoException {
+    const std::string nomeArquivo;
+};
+
+struct ErroDeFormatoDeArquivoException {
+    const int linha;
+    const std::string conteudo;
+};
+// ---------------------------------- assinatura --------------------------------- //
+struct NomeAssinaturaInvalidoException {};
+
+struct ValorAssinaturaInvalidoException {
+    const double valor;
+};
+
+struct DiaRenovacaoInvalidoException {
+    const int dia;
+};
+// ---------------------------------- assinatura --------------------------------- //
+#endif // ERROS_H
