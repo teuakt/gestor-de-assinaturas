0bf7d9cf36271ea8dce06aff741ae61451c25bcb
teuakt <mateushfsouza2@gmail.com>
2025-06-28 11:54:37 -0300

    test(lembrete): add coverage for exception path capture

--- tests/test_lembrete.cpp
+++ tests/test_lembrete.cpp
@@ -1,29 +1,61 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../external/doctest.h"
+#include "../include/lembrete.h"
+#include "../include/usuario.h"
+#include "../include/erros.h"
 
-#include "lembrete.h"
-#include "usuario.h"
-#include "assinatura.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <ctime>
 
-#include <vector>
+int calcularDiaFuturo(int diaAtual, int diasAMais) {
 
-TEST_CASE("Teste básico de Lembrete::verificarRenovacoes") {
+    time_t agora = time(0);
+    tm* ltm = localtime(&agora);
+    ltm->tm_mday += diasAMais;
+    mktime(ltm);
+    return ltm->tm_mday;
+}
+
+
+TEST_CASE("Testando a classe Lembrete") {
     Usuario usuario;
 
     time_t tempoAtual = time(nullptr);
     tm* tempoLocal = localtime(&tempoAtual);
     int diaAtual = tempoLocal->tm_mday;
 
-    Assinatura a1("Netflix", 29.90, diaAtual + 1);
-    Assinatura a2("Spotify", 19.90, diaAtual + 5);
-    Assinatura a3("Amazon Prime", 14.90, diaAtual + 10);
+    SUBCASE("verificarRenovacoes lança exceção se a lista de assinaturas estiver vazia") {
+        Lembrete lembrete(usuario);
+        CHECK_THROWS_AS(lembrete.verificarRenovacoes(7), ListaDeAssinaturasVaziaException);
+    }
 
-    usuario.adicionarAssinatura(a1);
-    usuario.adicionarAssinatura(a2);
-    usuario.adicionarAssinatura(a3);
+    SUBCASE("verificarRenovacoes identifica corretamente as renovações próximas") {
 
-    Lembrete lembrete(usuario);
+        Assinatura a_proxima("Netflix", 39.90, calcularDiaFuturo(diaAtual, 2)); 
+        Assinatura a_limite("Spotify", 19.90, calcularDiaFuturo(diaAtual, 7)); 
+        Assinatura a_distante("Disney+", 29.90, calcularDiaFuturo(diaAtual, 10)); 
 
-    lembrete.verificarRenovacoes(7);
+        usuario.adicionarAssinatura(a_proxima);
+        usuario.adicionarAssinatura(a_limite);
+        usuario.adicionarAssinatura(a_distante);
 
+        Lembrete lembrete(usuario);
+
+        std::stringstream buffer_captura;
+        auto* backup_cout = std::cout.rdbuf(); 
+        std::cout.rdbuf(buffer_captura.rdbuf()); 
+
+        lembrete.verificarRenovacoes(7);
+
+        std::cout.rdbuf(backup_cout);
+
+        std::string saida_capturada = buffer_captura.str();
+
+        CHECK(saida_capturada.find("Atenção! A assinatura \"Netflix\"") != std::string::npos);
+        CHECK(saida_capturada.find("Atenção! A assinatura \"Spotify\"") != std::string::npos);
+        
+        CHECK(saida_capturada.find("Atenção! A assinatura \"Disney+\"") == std::string::npos);
+    }
 }
